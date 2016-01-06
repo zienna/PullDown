4a5a34f7dc2fbe1f5d3a1adbfc2f529cb83c4bd0
William Willis <williswm@mail.uc.edu>
2016-01-05 19:10:21 -0500

    Added PullDownProject Method in Main Activity

--- app/src/main/java/example/pulldown/MainActivity.java
+++ app/src/main/java/example/pulldown/MainActivity.java
@@ -49,4 +49,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+    public void PullDownProject(){
+        int i = 1+1;
+    }
 }
